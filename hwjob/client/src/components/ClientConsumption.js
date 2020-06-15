f61df07b6a4ac56216cc219bbf24caf6f9f887ad
Michel Ho <mho85@icloud.com>
2020-06-15 14:54:24 +0200

    [F-E] Styling ClientConsumption

--- hwjob/client/src/components/ClientConsumption.js
+++ hwjob/client/src/components/ClientConsumption.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ResponsiveContainer, AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area, Label } from 'recharts';
+import { ResponsiveContainer, AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area } from 'recharts';
+import '../styles/ClientConsumption.css';
 
 import consumptionData from '../data.json';
 
@@ -21,21 +22,19 @@ class ClientConsumption extends Component {
         const colorMain = "#1fa5d7";
 
         return (
-            <div>
-                <h2>Last year's consumption</h2>
-                <ResponsiveContainer width="95%" height={400}>
+            <div id="client-consumption">
+                <h4>{lastYear} CONSUMPTION (KWh)</h4>
+                <ResponsiveContainer width="95%" height={350}>
                     <AreaChart width={730} height={250} data={data}
-                        margin={{ top: 10, right: 30, left: 30, bottom: 0 }}>
+                        margin={{ top: 10, right: 7, left: 7, bottom: 0 }}>
                         <defs>
                             <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
                                 <stop offset="5%" stopColor={colorMain} stopOpacity={0.8} />
                                 <stop offset="95%" stopColor={colorMain} stopOpacity={0} />
                             </linearGradient>
                         </defs>
-                        <XAxis dataKey="month">
-                            <Label value="MONTH" offset={0} position="insideBottom" />
-                        </XAxis>
-                        <YAxis label={{ value: "CONSUMPTION (KWH)", angle: -90, position: "insideLeft" }} stroke={colorMain} />
+                        <XAxis dataKey="month" />
+                        <YAxis stroke={colorMain} />
                         <CartesianGrid strokeDasharray="5 5" />
                         <Tooltip />
                         <Area type="monotone" dataKey="kwh_consumed" stroke={colorMain} fillOpacity={1} fill="url(#color)" />
