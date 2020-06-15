b731c8a80dafdfb21c82a9f495b17ee0f8c95c9a
Michel Ho <mho85@icloud.com>
2020-06-15 11:49:14 +0200

    [F-E] Styling SearchClient

--- hwjob/client/src/components/SearchClient.js
+++ hwjob/client/src/components/SearchClient.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
+import '../styles/SearchClient.css';
 
 import { setQuery } from '../actions';
 
+
 const SearchClient = (props) => {
     // console.log("SearchClient.js", props)
+
     return (
-        <div>
-            <p>Look for a client</p>
+        <div id="SearchClient">
             <TextField
-                label="Name / ID"
+                label="Enter name or ID"
                 type="text"
                 onChange={(event) => props.setQuery(event.target.value)}
             />
